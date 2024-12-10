1bbbbb667e976d2935d69c5869b8f3cde774b2ae
Stefan <git@stefn.co.uk>
2024-12-10 19:50:56 +0000

    Simpler flake

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     ruby-nix.url = "github:inscapist/ruby-nix";
+    ruby-nix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs =
@@ -20,29 +21,8 @@
         }:
         let
           cfg = config.services.railsApps;
-
-          fetchGitRepo =
-            url: ref: hash:
-            pkgs.fetchgit (
-              {
-                inherit url;
-                rev = ref;
-              }
-              // lib.optionalAttrs (hash != null) {
-                sha256 = hash;
-              }
-            );
-
-          # Use ruby-nix overlay
-          pkgsWithOverlay = import nixpkgs {
-            inherit (pkgs) system;
-            overlays = [ ruby-nix.overlays.ruby ];
-          };
-
-          rubyNix = ruby-nix.lib pkgsWithOverlay;
         in
         {
-          # Options remain the same
           options.services.railsApps = lib.mkOption {
             type =
               with lib.types;
@@ -54,25 +34,10 @@
                     example = "myapp.com";
                     description = "Domain name for the Rails application";
                   };
-                  gitUrl = lib.mkOption {
-                    type = str;
-                    example = "git@github.com:username/repo.git";
-                    description = "Git URL for the Rails application";
-                  };
-                  gitRef = lib.mkOption {
-                    type = str;
-                    default = "main";
-                    description = "Git reference (branch, tag, or commit)";
-                  };
                   useCaddy = lib.mkOption {
                     type = bool;
                     default = true;
                     description = "Whether to configure Caddy for this app";
-                  };
-                  sha256 = lib.mkOption {
-                    type = lib.types.nullOr lib.types.str;
-                    default = null;
-                    description = "Optional SHA256 hash of the git repository";
                   };
                   port = lib.mkOption {
                     type = lib.types.int;
@@ -95,13 +60,11 @@
                 lib.mkIf appConfig.enable {
                   "rails-${appConfig.domain}" =
                     let
-                      railsApp = fetchGitRepo appConfig.gitUrl appConfig.gitRef appConfig.sha256;
-
                       # Use ruby-nix to create the environment
                       rubyEnv = (
-                        rubyNix {
+                        ruby-nix.lib pkgs {
                           name = "rails-${appConfig.domain}";
-                          gemset = "${railsApp}/gemset.nix";
+                          gemset = ./gemset.nix;
                         }
                       );
 
@@ -111,7 +74,7 @@
                         export RAILS_ENV=production
                         export DOMAIN=${appConfig.domain}
 
-                        cd ${railsApp}
+                        cd ${self}
                         ${rubyEnv.env}/bin/bundle exec rails server -p ${toString appConfig.port}
                       '';
                     in
@@ -131,7 +94,7 @@
                         Type = "simple";
                         User = "rails-${appConfig.domain}";
                         Group = "rails-${appConfig.domain}";
-                        WorkingDirectory = "${railsApp}";
+                        WorkingDirectory = "${self}";
                         ExecStart = "${startScript}/bin/start-${appConfig.domain}";
                         Restart = "always";
                         RestartSec = "10";
@@ -142,7 +105,7 @@
               ) cfg
             );
 
-            # Users, groups, and Caddy configuration remain the same
+            # Users and groups configuration
             users.users = lib.mkMerge (
               lib.mapAttrsToList (
                 name: appConfig:
@@ -166,13 +129,14 @@
               ) cfg
             );
 
+            # Caddy configuration
             services.caddy.virtualHosts = lib.mkMerge (
               lib.mapAttrsToList (
                 name: appConfig:
                 lib.mkIf (appConfig.enable && appConfig.useCaddy) {
                   ${appConfig.domain} = {
                     extraConfig = ''
-                      root * ${fetchGitRepo appConfig.gitUrl appConfig.gitRef appConfig.sha256}/public
+                      root * ${self}/public
                       encode gzip
                       file_server /assets/*
                       reverse_proxy localhost:${toString appConfig.port} {
