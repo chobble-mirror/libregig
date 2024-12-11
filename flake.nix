5e468f430b21bf106670a43f097a2f7f06a53543
Stefan <git@stefn.co.uk>
2024-12-11 02:27:29 +0000

    Switch to pinned packages and stable nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
   description = "Libregig - band management app";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    ruby-nix.url = "github:inscapist/ruby-nix";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    ruby-nix.url = "github:inscapist/ruby-nix/1f3756f8a713171bf891b39c0d3b1fe6d83a4a63?narHash=sha256-CySXGzqycagfrWdcTwzM3Zo1MMm9uUtePYER9BvrW8s%3D";
+    flake-utils.url = "github:numtide/flake-utils/11707dc2f618dd54ca8739b309ec4fc024de578b?narHash=sha256-l0KFg5HjrsfsO/JpG%2Br7fRrqm12kzFHyUHqHCVpMMbI%3D";
   };
 
   outputs =
